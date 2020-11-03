f4873ed970f712ac9dda04947e49605a14d23c26
nakahararuu <repika02@gmail.com>
2020-11-03 23:15:37 +0900

    make vi mode able to accepting autosuggestions
    https://github.com/fish-shell/fish-shell/issues/3541

---
+++ private_dot_config/private_fish/private_functions/fish_user_key_bindings.fish
@@ -0,0 +1,11 @@
+function fish_user_key_bindings
+  # by `$(brew --prefix)/opt/fzf/install`
+  # https://github.com/junegunn/fzf#using-homebrew-or-linuxbrew
+  fzf_key_bindings
+
+  # for accepting autosuggestions in vi mode
+  # https://github.com/fish-shell/fish-shell/issues/3541#issuecomment-260001906
+  for mode in insert default visual
+    bind -M $mode \cf forward-char
+  end
+end
