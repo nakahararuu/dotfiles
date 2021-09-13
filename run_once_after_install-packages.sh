42202dbd9c4c0e0d58853fe0a2c7e1b46b1d7ffa
nakahararuu <repika02@gmail.com>
2021-09-13 15:39:11 +0900

    modify install script
    update tmux plugins when executing chezmoi apply

--- run_once_after_install-packages.sh
+++ run_once_after_install-packages.sh
@@ -83,10 +83,18 @@ fish -c "complete -c cheat.sh -xa '(curl -s cheat.sh/:list)'"
 
 # tmux plugins
 # see https://github.com/tmux-plugins/tpm/issues/6
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
-tmux start-server && \
-tmux new-session -d && \
-sleep 1 && \
-~/.tmux/plugins/tpm/scripts/install_plugins.sh && \
+if [ -e ~/.tmux/plugins/tpm ]; then
+    cd ~/.tmux/plugins/tpm && git pull && cd - && \
+    tmux start-server && \
+    tmux new-session -d && \
+    sleep 1 && \
+    ~/.tmux/plugins/tpm/scripts/update_plugin.sh
+else
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
+    tmux start-server && \
+    tmux new-session -d && \
+    sleep 1 && \
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+fi
 tmux kill-server
 
