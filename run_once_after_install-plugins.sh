fa54eccc81bee0a55dcacc2225cee8d3037c5825
nakahararuu <repika02@gmail.com>
2025-03-29 10:25:11 +0900

    refactor: tpm initialize snippet

--- run_once_after_install-plugins.sh
+++ run_once_after_install-plugins.sh
@@ -16,20 +16,3 @@ asdf install
 # cheat.sh
 fish -c "complete -c cheat.sh -xa '(curl -s cheat.sh/:list)'"
 
-# tmux plugins
-# see https://github.com/tmux-plugins/tpm/issues/6
-if [ -e ~/.config/tmux/plugins/tpm ]; then
-    cd ~/.config/tmux/plugins/tpm && git pull && cd - && \
-    tmux start-server && \
-    tmux new-session -d && \
-    sleep 1 && \
-    ~/.config/tmux/plugins/tpm/scripts/update_plugin.sh
-else
-    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm && \
-    tmux start-server && \
-    tmux new-session -d && \
-    sleep 1 && \
-    ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
-fi
-tmux kill-server
-
