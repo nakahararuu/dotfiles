4b99bbb61212b78aac1eb709fa1be5d2bf15bd8e
nakahararuu <repika02@gmail.com>
2025-03-29 10:25:28 +0900

    fix: wrong asdf sub command

--- run_once_after_install-plugins.sh
+++ run_once_after_install-plugins.sh
@@ -7,9 +7,9 @@ fish -c "curl -sL git.io/fisher | source && fisher update"
 # asdf plugin & global npm packages
 set +e
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add java https://github.com/halcyon/asdf-java.git
-asdf plugin-add python
-asdf plugin-add golang
+asdf plugin add java https://github.com/halcyon/asdf-java.git
+asdf plugin add python
+asdf plugin add golang
 set -e
 asdf install
 
