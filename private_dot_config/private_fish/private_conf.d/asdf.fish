8c65511200212eec8711f15d40ba914c0ce9a9cc
nakahararuu <repika02@gmail.com>
2025-05-25 17:22:28 +0900

    fix: asdf bug
    https://github.com/asdf-vm/asdf/issues/1895

--- private_dot_config/private_fish/private_conf.d/asdf.fish
+++ private_dot_config/private_fish/private_conf.d/asdf.fish
@@ -1,24 +1,9 @@
-# ASDF configuration code
+# ASDF configuration
 
-# https://github.com/asdf-vm/asdf/issues/1895
-# source (brew --prefix asdf)/share/fish/vendor_completions.d/asdf.fish
-
+# config locations
 set -gx ASDF_CONFIG_FILE "$HOME/.config/asdf/asdfrc"
-
-# shims path
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-if not contains $_asdf_shims $PATH
-    # Do not use fish_add_path (added in Fish 3.2) because it
-    # potentially changes the order of items in PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
+set -gx ASDF_NPM_DEFAULT_PACKAGES_FILE "$HOME/.config/asdf/default-npm-packages"
 
 # misc
-set -gx ASDF_NPM_DEFAULT_PACKAGES_FILE "$HOME/.config/asdf/default-npm-packages"
 set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
 
