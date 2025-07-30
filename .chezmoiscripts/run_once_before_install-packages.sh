72bb9c5f288e6c1a001394952c1efbbbdb78488f
nakahararuu <repika02@gmail.com>
2025-07-30 22:02:11 +0900

    merge both branch, master and private_machine

--- .chezmoiscripts/run_once_before_install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use bizBrewfile from chezmoi source directory directly
-BREWFILE_PATH="$(chezmoi source-path)/bizBrewfile"
-
-brew bundle --file="$BREWFILE_PATH"
-brew bundle cleanup --file="$BREWFILE_PATH" --force
-
-exit 0
