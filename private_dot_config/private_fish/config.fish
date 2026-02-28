44c0d2170ff2f6d94ce3a49f125be737fa07f6da
nakahararuu <repika02@gmail.com>
2026-02-28 16:12:42 +0900

    update node version for claude code

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -11,7 +11,7 @@ alias cat='bat'                                           # bat
 alias vi='nvim' && alias vim='nvim'                       # neovim
 alias ivi='ideae' && alias ivim='ideae'                   # Intellij IDEA LiteEditMode
 alias gemini='ASDF_NODEJS_VERSION=20.18.1 command gemini' # gemini-cli
-alias claude='ASDF_NODEJS_VERSION=18.17.0 command claude' # claude
+alias claude='ASDF_NODEJS_VERSION=20.18.1 command claude' # claude
 
 ## ls colors
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
