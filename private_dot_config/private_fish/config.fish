052068d994e575f4bcbff8190540ff50d107ec9b
nakahararuu <repika02@gmail.com>
2026-02-28 16:12:26 +0900

    remove github copilot old version

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,8 +10,6 @@ alias rm='ASDF_NODEJS_VERSION=20.18.1 trash'              # trash-cli
 alias cat='bat'                                           # bat
 alias vi='nvim' && alias vim='nvim'                       # neovim
 alias ivi='ideae' && alias ivim='ideae'                   # Intellij IDEA LiteEditMode
-alias ghcs='gh copilot suggest'                           # suggest CLI command with github colipot
-alias ghce='gh copilot explain'                           # explain CLI command with github colipot
 alias gemini='ASDF_NODEJS_VERSION=20.18.1 command gemini' # gemini-cli
 alias claude='ASDF_NODEJS_VERSION=18.17.0 command claude' # claude
 
